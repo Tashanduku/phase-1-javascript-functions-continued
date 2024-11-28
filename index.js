9d4a326e41bdf1380abba7196b5f0b542bebe748
Natasha Nduku <natashanduku0@gmail.com>
2024-11-28 08:17:42 +0300

    scope functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  
